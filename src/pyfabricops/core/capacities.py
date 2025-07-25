27ee4d8d8d6d7b2bbadc299f85971fe4b2d2e251
alisonpezzott <alisonpezzott@gmail.com>
2025-07-25 10:57:14 -0300

    refactor[capacities]: Tests passed.

--- src/pyfabricops/core/capacities.py
+++ src/pyfabricops/core/capacities.py
@@ -25,7 +25,7 @@ def list_capacities(
         (Union[DataFrame, List[Dict[str, Any]], None]): A DataFrame with the list of capacities.
         If `df=False`, returns a list of dictionaries. If no capacities are found, returns None.
     """
-    return api_request('/capacities', support_pagination=True) 
+    return api_request('/capacities', support_pagination=True)
 
 
 def get_capacity_id(capacity_name: str) -> str | None:
